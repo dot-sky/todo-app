d432751e1bf7e0799faf3129e29eee11698ac3ac
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-29 08:42:18 -0400

    Create Task class with core fields and methods for console testing.

---
+++ src/task.js
@@ -0,0 +1,32 @@
+import { format } from "date-fns";
+export class Task {
+  static #id = 0;
+  #projectId;
+  constructor(title, desc, dueDate, priority, status, projectId, checklist) {
+    this.title = title || "";
+    this.desc = desc || "";
+    this.dueDate = this.#formatDate(dueDate);
+    this.priority = priority || 0;
+    this.status = status || 0;
+    this.#projectId = projectId || 0;
+    this.checklist = checklist || [];
+
+    this.taskId = Task.#assignId();
+  }
+
+  #formatDate(date) {
+    return format(date || Date(Date.now()), "yyyy-MM-dd");
+  }
+
+  static #assignId() {
+    return Task.#id++;
+  }
+
+  getProjectId() {
+    return this.#projectId;
+  }
+
+  log() {
+    console.log(this);
+  }
+}
