6b1ffa8b7f713262c201f4a9b0c1d167298902bd
dot-sky <108539845+dot-sky@users.noreply.github.com>
2025-01-30 08:42:58 -0400

    Reorder methods of class.

--- src/task.js
+++ src/task.js
@@ -2,20 +2,17 @@ import { format } from "date-fns";
 export class Task {
   static #id = 0;
   #projectId;
+  #taskId;
   constructor(title, desc, dueDate, priority, status, projectId, checklist) {
     this.title = title || "";
     this.desc = desc || "";
-    this.dueDate = this.#formatDate(dueDate);
+    this.dueDate = this.setDueDate(dueDate);
     this.priority = priority || 0;
     this.status = status || 0;
     this.#projectId = projectId || 0;
     this.checklist = checklist || [];
 
-    this.taskId = Task.#assignId();
-  }
-
-  #formatDate(date) {
-    return format(date || Date(Date.now()), "yyyy-MM-dd");
+    this.#taskId = Task.#assignId();
   }
 
   static #assignId() {
@@ -26,11 +23,26 @@ export class Task {
     this.#projectId = projectId;
   }
 
+  // settters
+  setDueDate(date) {
+    return format(date || Date(Date.now()), "yyyy-MM-dd");
+  }
+
+  // getters
+  getId() {
+    return this.#taskId;
+  }
+
   getProjectId() {
     return this.#projectId;
   }
 
+  // console
   log() {
     console.log(this);
+  }
+
+  logSummary() {
+    console.log(` - ${this.#taskId} ${this.title} - ${this.dueDate}`);
   }
 }
